4fa48fa6af4c666e97ee22fdff921cc7d1055211
felipecarvalho <felipe.carvalho_2016@outlook.com>
2024-05-09 18:54:03 -0300

    firts commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
