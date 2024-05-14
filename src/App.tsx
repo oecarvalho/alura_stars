748fa4989392e9fd52e16844036a24344513e574
felipecarvalho <felipe.carvalho_2016@outlook.com>
2024-05-14 19:32:02 -0300

    iniciando projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import './App.css'
+import styled from 'styled-components';
+import EstilosGlobais from './assets/components/EstilosGlobais';
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  min-height: 100vh;
+`
 
 export default function App() {
 
   return (
-    <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <FundoGradiente>
+    <EstilosGlobais/>
+   </FundoGradiente>
   )
 }
